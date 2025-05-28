1428a8a7b138f9d0e23c757c2a4449b3780dd930
Richard <olella93@gmail.com>
2025-05-28 11:36:03 +0300

    Implement Artist, Album, and Track models with SQLAlchemy relationships, DB setup

---
+++ lib/db/models.py
@@ -0,0 +1,38 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, sessionmaker, declarative_base
+
+Base = declarative_base()
+
+class Artist(Base):
+    __tablename__ = 'artists'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    genre = Column(String)
+
+    albums = relationship('Album', back_populates='artist')
+
+class Album(Base):
+    __tablename__ = 'albums'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+
+    artist = relationship('Artist', back_populates='albums')
+    tracks = relationship('Track', back_populates='album')
+
+class Track(Base):
+    __tablename__ = 'tracks'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    album_id = Column(Integer, ForeignKey('albums.id'))
+    duration = Column(Integer) 
+
+    album = relationship('Album', back_populates='tracks')
+
+# DB setup
+engine = create_engine('sqlite:///mymusic.db')
+Session = sessionmaker(bind=engine)
+session = Session()
